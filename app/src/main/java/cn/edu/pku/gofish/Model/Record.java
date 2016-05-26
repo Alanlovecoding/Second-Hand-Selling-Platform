21e55db510e7711f79dc316bce0a6501b0d54d62
Iris <2940199214@qq.com>
2016-05-26 16:44:25 +0800

    add record upload file

--- app/src/main/java/cn/edu/pku/gofish/Model/Record.java
+++ app/src/main/java/cn/edu/pku/gofish/Model/Record.java
@@ -1,18 +1,68 @@
 package cn.edu.pku.gofish.Model;
 
+import android.preference.PreferenceActivity;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Created by leonardo on 16/5/11.
  */
 public class Record {
-    String text;
-    public Record(String _text)
+    private String title;
+    private String describetext;
+    private String price;
+    private String type;
+    private ArrayList<String> imagePaths;
+    private int imgcnt;
+    private String usrname;
+    private File[] file;
+    AsyncHttpClient client=new AsyncHttpClient();
+
+    public Record(String _usrname,String _title,String _describetext,String _pricetext,String _type,ArrayList<String> _imagePaths)
     {
-        text = _text;
+        title=_title;
+        describetext=_describetext;
+        price=_pricetext;
+        type=_type;
+        imagePaths=_imagePaths;
+        usrname=_usrname;
+        //file=new File[9];
+        imgcnt=imagePaths.size();
     }
 
-    public String TextLine()
-    {
-        return text;
-    }
+    public void uploadFile(String url) throws Exception {
+        int count =0;
+        RequestParams params = new RequestParams();
+        for(String tmp:imagePaths) {
+            file[count] = new File(tmp);
+            if (file[count].exists() && file[count].length() > 0) {
+                count++;
+            } else {
+                throw new Exception("FileNotFoundException");
+            }
+        }
+        params.put("uploadfiles",file);
+        params.put("price",price);
+        params.put("title",title);
+        params.put("type",type);
+        params.put("usrname",usrname);
+        params.put("describetext",describetext);
+        client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, PreferenceActivity.Header[] headers, byte[] bytes) {
 
-}
+            }
+
+            @Override
+            public void onFailure(int i, PreferenceActivity.Header[] headers, byte[] bytes, Throwable throwable) throws Exception{
+                throw new Exception("UploadFailException");
+            }
+            });
+    }
